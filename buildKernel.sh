de36ba22b8f9c87b3307d58471b1f151f48cd826
Hicham bchina <129168717+hicham-arch@users.noreply.github.com>
2026-06-19 21:45:12 +0100

    Change make command to use parallel jobs

--- buildKernel.sh
+++ buildKernel.sh
@@ -69,6 +69,6 @@ export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a31_sukiSu_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+make -j$(nproc) -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 #cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
